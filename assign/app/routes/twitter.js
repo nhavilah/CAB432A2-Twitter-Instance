aa09b1cc2a2cb004c6abeda49cd4b070f9e16644
nhavilah <51215537+nhavilah@users.noreply.github.com>
2021-10-25 03:34:33 +1000

    Mostly working persistence

--- assign/app/routes/twitter.js
+++ assign/app/routes/twitter.js
@@ -8,6 +8,33 @@ var Twit = require('twit');
 const server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
+const responseTime = require('response-time');
+const redis = require('redis');
+
+require('dotenv').config();
+const AWS = require('aws-sdk');
+
+// Cloud services setup
+// create unique bucket name
+const bucketName = 'nhavilah-cab432-twitter-storage';
+
+// create a promise on S3 service object
+const bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise();
+bucketPromise.then(function (data) {
+    console.log("Successfully created " + bucketName);
+}).catch(function (err) {
+    console.error(err, err.stack);
+});
+
+// create and connect redis client to local instance.
+const redisClient = redis.createClient();
+
+redisClient.on('error', (err) => {
+    console.log("Error " + err);
+});
+
+// use response-time as a middleware
+app.use(responseTime());
 
 var T = new Twit({
     consumer_key:         'pXoX1DPcMooGje3jECA4ijHDR',
@@ -22,6 +49,11 @@ router.post('/:number', (req,res) => {
     //keep the tweets array as a public item so we can apppend all tweets at to it
     const tweetArray=[]
     let tweets = req.body.tweets
+    const currentDate = new Date();
+    const currentDayOfMonth = currentDate.getDate();
+    const currentMonth = currentDate.getMonth(); // Be careful! January is 0, not 1
+    const currentYear = currentDate.getFullYear();
+    const timestamp = currentDayOfMonth+'-'+currentMonth+'-'+currentYear
     for(let i = 0; i < tweets.length; i++) {
         T.get('search/tweets', { q:tweets[i] , count: req.params.number }, function(err, data, response) {
             for (let index = 0; index < data.statuses.length; index++) {
@@ -29,12 +61,48 @@ router.post('/:number', (req,res) => {
                 tweetArray.push(x)
            }
            if(tweetArray.length === tweets.length*req.params.number){
-            res.set('Access-Control-Allow-Origin', '*');
-            res.write(JSON.stringify(tweetArray));
-            res.end();
+            let tweetBody = tweetArray
+            const redisKey = `twitter:${timestamp}`;
+            const s3Key = `twitter-${timestamp}`;
+
+            //try the cache
+            return redisClient.get(redisKey,(err,result) => {
+                if(result) {
+                    //serve from cache
+                    const resultJSON = JSON.parse(result);
+                    return res.status(200).json(resultJSON);
+                }else{
+
+                    //check s3
+                    const params = { Bucket: bucketName, Key: s3Key};
+                    //check s3 and serve if it exists in there
+                    return new AWS.S3({apiVersion: '2006-03-01'}).getObject(params, (err,result) => {
+                        if(result) {
+                            //serve from s3 and store in redis just in case
+                            redisClient.setex(redisKey,3600,JSON.stringify({source: 'Redis Cache',...tweetBody,}));
+                            console.log(result);
+                            const resultJSON = JSON.parse(result.Body);
+                            return res.status(200).json(resultJSON);
+                        } else {
+                            //serve from wikipedia api and store in s3 and redis
+                            const body = JSON.stringify({ source: 'S3 Bucket', ...tweetBody});
+                            const objectParams = {Bucket: bucketName, Key: s3Key, Body: body};
+                            const uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
+                            uploadPromise.then(function(data) {
+                                console.log("Successfully uploaded data to " + bucketName + "/" + s3Key);
+                            });
+                            redisClient.setex(redisKey,3600,JSON.stringify({source: 'Redis Cache',...tweetBody,}));
+                            res.write(JSON.stringify(tweetBody));
+                            return res.end();
+                        }
+                    })
+                }
+            });
            }
         })
     }
+
+    
     });
     
     
